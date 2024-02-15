022669f6c774cf47481f663b342146eee8719c48
Thiraput01 <Thiraputball@gmail.com>
2024-02-15 16:42:28 +0700

    login with cookies

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 
 
@@ -9,6 +10,8 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+
 const hospitals = require('./routes/hospitals');
 const auth = require('./routes/auth');
 
@@ -19,5 +22,6 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
 process.on('unhandledRejection', (err, promise)=>{
     console.log(`Error: ${err.message}`);
+
     server.close(()=> process.exit(1));
 })
