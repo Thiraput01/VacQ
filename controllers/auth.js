1f30226be3880aa44e964ff4a18ae2dd6ba46b30
Thiraput01 <Thiraputball@gmail.com>
2024-02-15 15:57:28 +0700

    add tokens

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,10 +14,42 @@ exports.register = async (req, res, next) =>{
             password,
             role
         }); 
-        res.status(200).json({success: true});
+        const token = user.getSignedJwtToken();
+        res.status(200).json({success: true, token});
     }catch (err){
         res.status(400).json({success: false});
         console.log(err.stack);
     }
    
+}
+
+//@desc    Login User
+//@route   POST /api/v1/auth/login
+//@access  Public
+exports.login = async (req, res, next) =>{
+    const {email, password} = req.body;
+
+    //Validate email and password
+    if(!email || !password){
+        return res.status(400).json({success: false,
+             msg:'Please provide an email and password'})
+    }
+
+    //check for user
+    const user = await User.findOne({email}.select('+password'));
+    if(!user){
+        return res.status(400).json({success: false,
+            msg:'Invalid credentials'});
+    }
+    //check if password matches
+    const isMatch = await user.matchPassword(password);
+    if(!isMatch){
+        return res.status(401).json({success: false,
+            msg:'Invalid credentials'});
+    }
+
+    //create token
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({success: true, token});
 }
--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
 const UserSchema= new mongoose.Schema({
     name: {
         type: String,
@@ -40,4 +43,16 @@ UserSchema.pre('save', async function(next){
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 });
+
+//Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function(){
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    })
+}
+
+//Match user password to hashed password in database
+UserSchema.methods.matchPassword = async function(){
+    return await bcrypt.compare(enteredPassword, this.password);
+}
 module.exports = mongoose.model('User', UserSchema);
