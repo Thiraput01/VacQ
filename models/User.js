aa85a93a7dd9fbcb4241fde6adc3b4ff476b0342
Thiraput01 <Thiraputball@gmail.com>
2024-02-15 15:27:11 +0700

    workinng on bcrypt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 const UserSchema= new mongoose.Schema({
     name: {
         type: String,
@@ -35,5 +35,9 @@ const UserSchema= new mongoose.Schema({
         default: Date.now
     }
 });
-
+//encrypt pass using bcrypt
+UserSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 module.exports = mongoose.model('User', UserSchema);
