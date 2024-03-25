7e76a0fc25017f5f84839f0ec60bf045b2f48e05
Thiraput01 <Thiraputball@gmail.com>
2024-03-25 22:38:35 +0700

    update

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const {getHospital, getHospitals, createHospital, updateHospital, deleteHospital} = require('../controllers/hospitals');
+const {getHospital, getHospitals, createHospital, updateHospital, deleteHospital, getVacCenters} = require('../controllers/hospitals');
+
+//include other resource routers
+const appointmentRouter = require('./appointments');
+
 const router = express.Router();
 
 const {protect, authorize} = require('../middleware/auth');
 
+//re-route into other resource rounters
+router.use('/:hospitalId/appointments/', appointmentRouter);
+
+router.route('/vacCenters').get(getVacCenters);
 router.route('/').get(getHospitals).post(protect, authorize('admin'), createHospital);
 router.route('/:id').get(getHospital).put(protect, authorize('admin'), updateHospital).delete(protect, authorize('admin'), deleteHospital);
 
--- server.js
+++ server.js
@@ -12,11 +12,15 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+//route files
 const hospitals = require('./routes/hospitals');
 const auth = require('./routes/auth');
+const appointments = require('./routes/appointments') 
 
+//mount routers
 app.use('/api/v1/hospitals', hospitals);
 app.use('/api/v1/auth', auth);
+app.use('/api/v1/appointments', appointments);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
